fa9eaf2fba1ab4c24873df199ee0f5895b0aaa96
hastytoro <luigi.lupini@gmail.com>
2022-11-28 16:09:27 +0200

    Basic setup and layout

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hulu clone</title>
+      </Head>
+      <h1>Here is the start of hulu clone</h1>
+    </div>
+  );
+}
