0c590b63f41d6fe70af7c14fd0294c922e7db055
Luigi Lupini <luigi.lupini@gmail.com>
2022-11-29 14:21:33 +0200

    #3 `getServerSideProps` content from "themoviedb"

---
+++ components/MainContent.js
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <div>
+      <h1>main content hello</h1>
+    </div>
+  );
+}
+
+export default MainContent;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
+import MainContent from "../components/MainContent";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -10,7 +12,26 @@ export default function Home() {
       </Head>
       <Header />
       <NavBar />
-      {/* MainResults */}
+      <MainContent />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  // We default if no route is selected & pushed via `NavBar.js` query param.
+  // We use the `query` data in our fetch to interpolate the needed API call.
+  const genre = context.query.genre;
+  console.log(context.query);
+  const req = await fetch(
+    `https://api.themoviedb.org/3${
+      /* # https://developer.mozilla.org/en-US/docs/Web/API/Response/json:
+      The json() method of the Response interface takes a Response stream and reads it to completion. It returns a promise which resolves with the result of parsing the body text as JSON. Note that despite the method being named json(), the result is not JSON, but instead the result of taking JSON as input, and parsing it to produce a JS object. */
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+  return {
+    props: {
+      results: req,
+    },
+  };
 }
